6651798f5212cecee47d4239887752efbf132c69
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-27 08:15:44 -0800

    Styled buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,11 +24,11 @@ export default function Home() {
     <main className="flex flex-col items-center my-10 gap-16">
       <h1 className="text-3xl font-semibold">How Much Have I Made</h1>
       <h2 className="text-3xl">Money</h2>
-      <div className="flex flex-col items-center">
-        <button onClick={startShift}>Start Shift</button>
-        <button onClick={startMeal}>Start Meal</button>
-        <button onClick={endMeal}>End Meal</button>
-        <button onClick={endShift}>End Shift</button>
+      <div className="flex flex-col items-center gap-5 text-lg font-semibold">
+        <button onClick={startShift} className="bg-blue-300 px-4 py-2 rounded-xl">Start Shift</button>
+        <button onClick={startMeal} className="bg-blue-300 px-4 py-2 rounded-xl">Start Meal</button>
+        <button onClick={endMeal} className="bg-blue-300 px-4 py-2 rounded-xl">End Meal</button>
+        <button onClick={endShift} className="bg-blue-300 px-4 py-2 rounded-xl">End Shift</button>
       </div>
     </main>
   )
