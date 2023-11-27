850d618c895541e0cebeee71ba6336d74196d46d
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-27 07:53:05 -0800

    Made HTML structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@
 
 export default function Home() {
   return (
-    <main>
-      <h1>Homepage</h1>
+    <main className="flex flex-col items-center my-10 gap-16">
+      <h1 className="text-3xl font-semibold">How Much Have I Made</h1>
+      <h2 className="text-3xl">Money</h2>
+      <div className="flex flex-col items-center">
+        <button>Start Shift</button>
+        <button>Start Meal</button>
+        <button>End Meal</button>
+        <button>End Shift</button>
+      </div>
     </main>
   )
 }
