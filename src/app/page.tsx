d5fab1121896c33cccb4762fcdd1ecea6a0fbe62
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-27 08:13:17 -0800

    Set up buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
-
+'use client'
 
 export default function Home() {
+
+
+
+  const startShift = () => {
+
+  }
+
+  const startMeal = () => {
+    
+  }
+
+  const endMeal = () => {
+    
+  }
+
+  const endShift = () => {
+    
+  }
+
   return (
     <main className="flex flex-col items-center my-10 gap-16">
       <h1 className="text-3xl font-semibold">How Much Have I Made</h1>
       <h2 className="text-3xl">Money</h2>
       <div className="flex flex-col items-center">
-        <button>Start Shift</button>
-        <button>Start Meal</button>
-        <button>End Meal</button>
-        <button>End Shift</button>
+        <button onClick={startShift}>Start Shift</button>
+        <button onClick={startMeal}>Start Meal</button>
+        <button onClick={endMeal}>End Meal</button>
+        <button onClick={endShift}>End Shift</button>
       </div>
     </main>
   )
