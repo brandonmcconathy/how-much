e28940ca873d73573fed7fb8b6c731cca95088ad
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-30 09:30:55 -0800

    Added id's to buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,10 +36,10 @@ export default function Home() {
       <h2 className="text-3xl">Money</h2>
       <input className="bg-blue-200 px-4 py-2 rounded-xl box-pop" placeholder="Hourly Rate" value={rate} onChange={handleChange} required />
       <div className="flex flex-col items-center gap-5 text-lg font-semibold">
-        <button onClick={startShift} className="bg-blue-300 px-4 py-2 rounded-xl">Start Shift</button>
-        <button onClick={startMeal} className="bg-blue-300 px-4 py-2 rounded-xl">Start Meal</button>
-        <button onClick={endMeal} className="bg-blue-300 px-4 py-2 rounded-xl">End Meal</button>
-        <button onClick={endShift} className="bg-blue-300 px-4 py-2 rounded-xl">End Shift</button>
+        <button id="start-shift" onClick={startShift} className="bg-blue-300 px-4 py-2 rounded-xl">Start Shift</button>
+        <button id="start-meal" onClick={startMeal} className="bg-blue-300 px-4 py-2 rounded-xl">Start Meal</button>
+        <button id="end-meal" onClick={endMeal} className="bg-blue-300 px-4 py-2 rounded-xl">End Meal</button>
+        <button id="end-shift" onClick={endShift} className="bg-blue-300 px-4 py-2 rounded-xl">End Shift</button>
       </div>
     </main>
   )
