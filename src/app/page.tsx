2a64117d015d0c86670cbc6c43be2c53a608d682
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-12-01 10:20:20 -0800

    Rate and times pull from database and display money made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 'use client'
 
-import { setDoc, updateDoc, doc } from "firebase/firestore"
+import { setDoc, updateDoc, doc, getDoc } from "firebase/firestore"
 import { db } from '../../lib/firebase'
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import moneyCalc from '../../utils/moneycalc'
 
 export default function Home() {
 
   const [rate, setRate] = useState('')
+  const [times, setTimes] = useState({start: '', lunch: '', endLunch: ''})
+  const [money, setMoney] = useState(0)
+
+  useEffect(() => {
+    const getDBData = async () => {
+      const timesSnap = await getDoc(doc(db, 'data', 'times'))
+      const rateSnap = await getDoc(doc(db, 'data', 'rate'))
+      const tempTimes = timesSnap.data()
+      const tempRate = rateSnap.data().rate
+      setTimes(tempTimes)
+      setRate(tempRate)
+      console.log(tempTimes)
+      setMoney(moneyCalc(tempTimes, tempRate))
+    }
+    getDBData()
+  }, [])
 
   const handleChange = (event: any) => {
     setRate(event.target.value)
@@ -33,7 +50,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center my-10 gap-16 text-white">
       <h1 className="text-3xl font-semibold">How Much Have I Made</h1>
-      <h2 className="text-3xl">Money</h2>
+      <h2 className="text-3xl">{money}</h2>
       <input className="bg-green-700 px-4 py-2 rounded-xl box-pop" placeholder="Hourly Rate" value={rate} onChange={handleChange} required />
       <div className="flex flex-col items-center gap-5 text-lg font-semibold">
         <button id="start-shift" onClick={startShift} className="bg-green-700 px-4 py-2 rounded-xl">Start Shift</button>
