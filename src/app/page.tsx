20b675864474da7c377e2202fb104fbf097b740b
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-12-02 14:01:29 -0800

    Starting adding different status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,26 @@ import moneyCalc from '../../utils/moneycalc'
 export default function Home() {
 
   const [rate, setRate] = useState('')
+  const [status, setStatus] = useState('')
   const [times, setTimes] = useState({start: '', lunch: '', endLunch: ''})
   const [money, setMoney] = useState(0)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const getDBData = async () => {
       const timesSnap = await getDoc(doc(db, 'data', 'times'))
       const rateSnap = await getDoc(doc(db, 'data', 'rate'))
+      const statusSnap = await getDoc(doc(db, 'data', 'status'))
       const tempTimes = timesSnap.data()
       const tempRate = rateSnap.data().rate
       setTimes(tempTimes)
       setRate(tempRate)
+      setStatus(statusSnap.data().status)
       console.log(tempTimes)
       setMoney(moneyCalc(tempTimes, tempRate))
     }
     getDBData()
+    setLoading(false)
   }, [])
 
   const handleChange = (event: any) => {
@@ -47,17 +52,44 @@ export default function Home() {
     
   }
 
-  return (
-    <main className="flex flex-col items-center my-10 gap-16 text-white">
-      <h1 className="text-3xl font-semibold">How Much Have I Made</h1>
-      <h2 className="text-3xl">${money}</h2>
-      <input className="bg-green-700 px-4 py-2 rounded-xl box-pop" placeholder="Hourly Rate" value={rate} onChange={handleChange} required />
-      <div className="flex flex-col items-center gap-5 text-lg font-semibold">
-        <button id="start-shift" onClick={startShift} className="bg-green-700 px-4 py-2 rounded-xl">Start Shift</button>
-        <button id="start-meal" onClick={startMeal} className="bg-green-700 px-4 py-2 rounded-xl">Start Meal</button>
-        <button id="end-meal" onClick={endMeal} className="bg-green-700 px-4 py-2 rounded-xl">End Meal</button>
-        <button id="end-shift" onClick={endShift} className="bg-green-700 px-4 py-2 rounded-xl">End Shift</button>
-      </div>
-    </main>
-  )
+  if (loading) {
+    return (
+      <main className="flex flex-col items-center my-10 gap-16 text-white">
+        <h1 className="text-3xl font-semibold">How Much Have I Made</h1>
+        <h1>Loading...</h1>
+      </main>
+    )
+  }
+
+  if (status == 'off') {
+    return (
+      <main className="flex flex-col items-center my-10 gap-16 text-white">
+        <h1 className="text-3xl font-semibold">How Much Have I Made</h1>
+        <h2 className="text-3xl">${money}</h2>
+        <input className="bg-green-700 px-4 py-2 rounded-xl box-pop" placeholder="Hourly Rate" value={rate} onChange={handleChange} required />
+        <div className="flex flex-col items-center gap-5 text-lg font-semibold">
+          <button id="start-shift" onClick={startShift} className="bg-green-700 px-4 py-2 rounded-xl">Start Shift</button>
+          <button id="start-meal" onClick={startMeal} className="bg-green-700 px-4 py-2 rounded-xl">Start Meal</button>
+          <button id="end-meal" onClick={endMeal} className="bg-green-700 px-4 py-2 rounded-xl">End Meal</button>
+          <button id="end-shift" onClick={endShift} className="bg-green-700 px-4 py-2 rounded-xl">End Shift</button>
+          <h1>{status}</h1>
+        </div>
+      </main>
+    )
+  }
+
+  if (status == 'working') {
+    return (
+      <main className="flex flex-col items-center my-10 gap-16 text-white">
+        <h1 className="text-3xl font-semibold">How Much Have I Made</h1>
+        <h2 className="text-3xl">${money}</h2>
+        <div className="flex flex-col items-center gap-5 text-lg font-semibold">
+          <button id="start-meal" onClick={startMeal} className="bg-green-700 px-4 py-2 rounded-xl">Start Meal</button>
+          <button id="end-meal" onClick={endMeal} className="bg-green-700 px-4 py-2 rounded-xl">End Meal</button>
+          <button id="end-shift" onClick={endShift} className="bg-green-700 px-4 py-2 rounded-xl">End Shift</button>
+          <h1>{status}</h1>
+        </div>
+      </main>
+    )
+  }
 }
