906ff97e1b97107d51d8beda90b7fc9548cec7ff
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-12-02 14:28:29 -0800

    Fixed time calc

--- utils/timecalc.js
+++ utils/timecalc.js
@@ -3,16 +3,16 @@ export default function timeCalc(times) {
   const currTime = Date.now() / 1000
 
   if (lunch == '') {
-    const time = (currTime - start) * 3600
+    const time = (currTime - start) / 3600
     return time.toFixed(2)
   } else if (endLunch == '') {
-    const time = (lunch - start) * 3600
+    const time = (lunch - start) / 3600
     return time.toFixed(2)
   } else if (endWork == '') {
-    const time = ((lunch - start) + (currTime - endLunch)) * 3600
+    const time = ((lunch - start) + (currTime - endLunch)) / 3600
     return time.toFixed(2)
   } else {
-    const time = ((lunch - start) + (endWork - endLunch)) * 3600
+    const time = ((lunch - start) + (endWork - endLunch)) / 3600
     return time.toFixed(2)
   }
 }
